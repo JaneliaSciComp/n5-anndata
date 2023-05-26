79826ef97f3ef98d11ff8f9aac3d905795fff8c5
Michael Innerberger <innerbergerm@hhmi.org>
2023-05-26 15:22:21 -0400

    Initialize maven project

---
+++ src/test/java/org/janelia/n5anndata/AppTest.java
@@ -0,0 +1,19 @@
+package org.janelia.n5anndata;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * A test.
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertEquals(0, 0);
+    }
+}

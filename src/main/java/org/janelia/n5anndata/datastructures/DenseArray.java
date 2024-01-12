b3b3d26fab0251db6a33a402649cc4fbc68b4b21
Michael Innerberger <innerbergerm@hhmi.org>
2024-01-12 15:30:18 -0500

    Delete unused DenseArray classes

--- src/main/java/org/janelia/n5anndata/datastructures/DenseArray.java
+++
@@ -1,8 +0,0 @@
-package org.janelia.n5anndata.datastructures;
-
-import net.imglib2.img.Img;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.NumericType;
-
-public interface DenseArray<T extends NumericType<T> & NativeType<T>> extends Img<T> {
-}
